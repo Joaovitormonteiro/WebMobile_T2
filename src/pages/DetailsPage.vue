2c17ee0a82063ab9ca88888f466e53e0c7dfc68f
Gustavo Perin <bobagi@usp.br>
2023-07-25 19:11:01 -0300

    Update DetailsPage.vue

--- src/pages/DetailsPage.vue
+++ src/pages/DetailsPage.vue
@@ -2,42 +2,51 @@
 .centered {
   display: flex;
   justify-content: center;
-  /* align-items: center; */
-  height: 100vh;
 }
 </style>
 <template>
-    <div class="centered">
+    <div class="centered">       
       <h1>{{ $route.params.name }} details</h1>
     </div>
-</template>
-
-<script lang="ts">
-import { defineComponent, ref, onMounted } from 'vue';
-import postsService from 'src/services/posts'
-
-export default defineComponent({
-  name: 'IndexPage',
-  setup(){
-    const posts = ref([])
-    const { list } = postsService('')
-
-    onMounted(() => {
-      getPosts()
-    })
-
-    const getPosts = async () => {      
-      try {         
-        posts.value = await list()      
-        console.log(posts.value)
-      } catch(error) {
-        console.log(error)
-      }      
+    <div class="centered">      
+      <q-img
+        :src="imageSrc"
+        spinner-color="white"
+        style="height: 280px; max-width: 300px"
+      />
+    </div>
+  </template>
+  
+  <script lang="ts">
+  import { defineComponent, ref, onMounted } from 'vue';
+  import postsService from 'src/services/posts'
+  
+  export default defineComponent({
+    name: 'DetailsPage',
+    setup(){
+      const posts = ref([]);
+      const imageSrc = ref(''); // Initialize imageSrc as a ref with an empty string
+  
+      onMounted(() => {          
+        const segments = window.location.href.split('/') 
+        const pokiName = segments.pop()            
+        getPosts(pokiName)
+      });
+  
+      const getPosts = async (pokemonName: string | undefined) => {      
+        try {         
+          const { list } = postsService(pokemonName)        
+          posts.value = await list()           
+          imageSrc.value = posts.value.sprites.front_default; // Update the value using .value
+        } catch(error) {
+          console.log(error)
+        }      
+      };
+  
+      return {
+        posts,
+        imageSrc        
+      };
     }
-
-    return{
-      posts
-    }
-  }
-})
-</script>
+  })
+  </script>  
